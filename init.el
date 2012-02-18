a9b5766174c6e95df2074a85848214a982b8a8bc
Troy Astorino <troyastorino@gmail.com>
2012-02-18 11:29:40 -0800

    added some initial files

---
+++ init.el
@@ -0,0 +1,50 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      starter-kit-js
+                      clojure-mode
+                      rainbow-delimiters
+                      auctex
+                      regex-tool
+                      markdown-mode
+                      js2-mode
+                      flymake-jshint)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Changed key mappings
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; Cosmetic changes
+(global-rainbow-delimiters-mode)
+(set-face-attribute 'default nil :height 100) ;10 pt font
+
+;; Set js2-mode to be default
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; Customizations for LaTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+(setq TeX-PDF-mode t) ;By default compile to PDF
